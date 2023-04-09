9b63929988edc998d29b02e8e23044a27e9b4863
James Donnelly <jrdonnelly468@gmail.com>
2023-04-09 14:51:33 -0700

    working version save

--- application/public/js/validation.js
+++ application/public/js/validation.js
@@ -1,16 +1,19 @@
-const username = document.getElementById("name");
-const password = document.getElementById("password");
-const confrim_password = document.getElementById("confirm password");
-const email = document.getElementById("email");
+const username = document.querySelector("username");
+const password = document.querySelector("password");
+const confrim_password = document.querySelector("confirm password");
+
 const alphanum = /^[a-zA-Z][a-zA-Z0-9]{2,}/;
 const firstchar= /^[a-zA-Z]+$/;
-function validation(){
-    if (username < 3 ){
-        alert("Name length should be 3+ characters long and start with A-Z, upper or lowercase");
-        username.focus();
-        return false;
-    }
 
+const chkUsername = () => {
+    let valid = false;
+    const min = 3;
+
+    const
     
-    return true;
+    if(!isRequired()){
+        showErr(username)
+    }
 }
+
+
