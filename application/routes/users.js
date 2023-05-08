b970e9b17416ac1a3f391a9d6a1e63f7382de699
James Donnelly <jrdonnelly468@gmail.com>
2023-05-07 18:32:24 -0700

    save point

--- application/routes/users.js
+++ application/routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET users listing. */
+var db = require('../conf/database ')
+/* GET localhost:3000. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  
 });
 
 router.post('/login', function(req, res, next){
