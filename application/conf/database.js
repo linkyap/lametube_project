3f80625892f5bc3b06144a4e3ba4d5bc10505418
James Donnelly <jrdonnelly468@gmail.com>
2023-05-09 17:17:25 -0700

    test commit

--- application/conf/database.js
+++ application/conf/database.js
@@ -14,4 +14,6 @@ const pool = mysql.createPool({
   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
   idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
   queueLimit: 0
-}); 
+}).promise(); 
+
+module.exports = pool;
--- application/routes/users.js
+++ application/routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../conf/database ')
-/* GET localhost:3000. */
-router.get('/', function(req, res, next) {
-  
+var db = require('../conf/database');
+/* GET localhost:3000/users */
+router.get('/', async function(req, res, next) {
+  try{
+    let [rows, fields] = await db.query(`select * from users;`
+    );
+    res.status(200).json({rows, fields})
+  }catch(error){
+    next(error);
+  }
 });
 
 router.post('/login', function(req, res, next){
